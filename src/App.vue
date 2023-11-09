5da6dcdefa06aeacbe90a65f6c2b0e09e621eb5a
shawn edgell <shawn_edgell@hotmail.com>
2023-11-09 14:33:47 -0500

    added burger menu transition, fixed bugs

--- src/App.vue
+++ src/App.vue
@@ -9,10 +9,11 @@
           ☰
         </button>
         <!-- Full Menu for Desktop and Expanded Menu for Mobile -->
-        <nav :class="['absolute top-full text-center left-0 w-full bg-neutral-900 flex-col md:static md:flex md:flex-row gap-4 justify-center flex-1', isMenuOpen ? 'pb-4 flex' : 'hidden']">
-            <router-link to="/" class="text-lg font-semibold">Home</router-link>
-            <router-link to="/login" class="text-lg font-semibold">Login</router-link>
-            <router-link to="/user-profile" class="text-lg font-semibold">Profile</router-link>
+        <nav :class="menuClasses">
+            <router-link to="/" class="text-lg font-semibold py-2 md:py-0">Home</router-link>
+            <router-link to="/login" class="text-lg font-semibold py-2 md:py-0">Login</router-link>
+            <!-- Added padding to the bottom for mobile view -->
+            <router-link to="/user-profile" class="text-lg font-semibold py-2 md:py-0 pb-4 md:pb-0">Profile</router-link>
         </nav>
       </div>
       <!-- Page Content -->
@@ -22,24 +23,33 @@
     </div>
   </template>
 
-  
-  <script>
-  export default {
-    name: 'App',
-    data() {
-      return {
-        isMenuOpen: false,
-      };
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      isMenuOpen: false,
+    };
+  },
+  computed: {
+    menuClasses() {
+      return [
+        'absolute gap-x-4 top-full left-0 w-full bg-neutral-900 md:static md:flex md:flex-row md:justify-center md:items-center',
+        'flex flex-col text-center transition-height duration-500 ease-in-out',
+        this.isMenuOpen ? 'max-h-screen' : 'max-h-0', // Control max height for slide effect
+        'overflow-hidden md:overflow-visible', // Prevent overflow during transition
+        'md:flex' // Always show menu in desktop view
+      ];
+    }
+  },
+  methods: {
+    toggleMenu() {
+      this.isMenuOpen = !this.isMenuOpen;
     },
-    methods: {
-      toggleMenu() {
-        this.isMenuOpen = !this.isMenuOpen;
-      },
-    },
-  };
-  </script>
-  
-  <style>
-  /* ... global styles if necessary ... */
-  </style>
-  
+  },
+};
+</script>
+
+<style>
+/* ... existing styles ... */
+</style>
